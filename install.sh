64a9f629bb2965cc12b00550a86679334bce8523
rangapv <rangapv@yahoo.com>
2020-05-11 07:23:54 +0000

    Added doker pre-requisites

--- install.sh
+++ install.sh
@@ -6,33 +6,46 @@ echo $li2
 
 u1=$(cat /etc/*-release | grep ubuntu)
 
-if [ ! -z "$u1" ]
-then 
-echo "hi"
-
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-
-
-if [ -f "/etc/apt/sources.list.d/kubernetes.list" ]
+dk=$(docker --version)
+ds=$?
+if [[ $ds == 0 ]]
 then
-		echo "kubernetes.list found\n"
-		sudo truncate -s 0 /etc/apt/sources.list.d/kubernetes.list 
+
+	if [ ! -z "$u1" ]
+	then 
+
+	sudo apt-get update && sudo apt-get install -y apt-transport-https
+
+	sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+
+
+		if [ -f "/etc/apt/sources.list.d/kubernetes.list" ]
+		then
+			echo "kubernetes.list found\n"
+			sudo truncate -s 0 /etc/apt/sources.list.d/kubernetes.list 
+		else
+			echo "Creating kubernetes list"
+			sudo touch /etc/apt/sources.list.d/kubernetes.list 
+		fi
+
+		sudo chmod 777 /etc/apt/sources.list.d/kubernetes.list 
+		sudo echo "deb http://apt.kubernetes.io/ kubernetes-$(lsb_release -cs) main" >> /etc/apt/sources.list.d/kubernetes.list
+
+	sudo apt-get update
+
+	sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+	sudo systemctl enable kubelet && sudo systemctl start kubelet
+
+        echo "ONE MORE THING ........."
+	echo "sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --token-ttl 0"
+        echo "execute the above command and captire the node tokens"
+
+	fi 
 else
-		echo "Creating kubernetes list"
-		sudo touch /etc/apt/sources.list.d/kubernetes.list 
+   echo "Pls install docker and re-run this Script"
 fi
 
-sudo chmod 777 /etc/apt/sources.list.d/kubernetes.list 
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
-
-sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
-
-
-
-fi 
 # end of $ui if
