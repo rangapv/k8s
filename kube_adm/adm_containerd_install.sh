002432d1c3913a498f58a1d5be681b5c313a1685
rangapv <rangapv@yahoo.com>
2021-06-19 16:07:29 +0000

    Added Ubuntu support and also config pull from gist for kubeadm init

--- kube_adm/adm_containerd_install.sh
+++ kube_adm/adm_containerd_install.sh
@@ -8,16 +8,6 @@ kubeblock() {
  sudo $cm1 update && sudo $cm1 install -y apt-transport-https ca-certificates
  sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo $add2 add -
 
- if [ -f "/etc/apt/sources.list.d/kubernetes.list" ]
- then
-	 echo "kuberneteslist found\n"
-	 sudo truncate -s 0 /etc/apt/sources.list.d/kubernetes.list
- else
-	 echo "creating kubernetes list\n"
-	 sudo touch /etc/apt/sources.list.d/kubernetes.list
- fi
-
- sudo chmod 777 /etc/apt/sources.list.d/kubernetes.list
  sudo curl -fsSLO /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
  sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
  sudo $cm1 update
@@ -42,7 +32,10 @@ then
 	then 
         kubeblock apt-key
         echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init  --config=./kube-adm-containerd.yaml --pod-network-cidr=10.244.0.0/16 --token-ttl 0 \n\n" | cowsay -W95 -f default
-       
+        elif [ ! -z "$u1" ]
+	then
+        kubeblock apt-key
+	echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init --config=\`wget https://gist.github.com/rangapv/3fd8a52f66bd412b1cc0663a45c74f68.git\` \n\n" | cowsay -W95 -f default
 	fi 
 else
    echo "Pls install containerd and re-run this Script"
