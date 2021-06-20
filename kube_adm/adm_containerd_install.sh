7afc18b9c713b0ae79ed073bfd2f87d830d04701
rangapv <rangapv@yahoo.com>
2021-06-20 06:23:48 +0000

    Fixed the repo link for kubeadm downloads

--- kube_adm/adm_containerd_install.sh
+++ kube_adm/adm_containerd_install.sh
@@ -8,8 +8,8 @@ kubeblock() {
  sudo $cm1 update && sudo $cm1 install -y apt-transport-https ca-certificates
  sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo $add2 add -
 
- sudo curl -fsSLO /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
- sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
  sudo $cm1 update
  sudo $cm1 install -y kubectl kubeadm kubelet
  sudo apt-mark hold kubelet kubeadm kubelet
