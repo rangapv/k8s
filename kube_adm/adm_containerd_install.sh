1b35a5cbd095c6ff46dc24da87bc50dd4d49b3a7
rangapv <rangapv@yahoo.com>
2021-06-20 06:33:09 +0000

    Removed the apt-key command line argument

--- kube_adm/adm_containerd_install.sh
+++ kube_adm/adm_containerd_install.sh
@@ -6,8 +6,6 @@ kubeblock() {
 
  add2="$@"
  sudo $cm1 update && sudo $cm1 install -y apt-transport-https ca-certificates
- sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo $add2 add -
-
  sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
  echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
  sudo $cm1 update
@@ -30,11 +28,11 @@ then
 
 	if [ ! -z "$d1" ]
 	then 
-        kubeblock apt-key
+        kubeblock
         echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init  --config=./kube-adm-containerd.yaml --pod-network-cidr=10.244.0.0/16 --token-ttl 0 \n\n" | cowsay -W95 -f default
         elif [ ! -z "$u1" ]
 	then
-        kubeblock apt-key
+        kubeblock 
 	echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init --config=\`wget https://gist.github.com/rangapv/3fd8a52f66bd412b1cc0663a45c74f68.git\` \n\n" | cowsay -W95 -f default
 	fi 
 else
