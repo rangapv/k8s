a32425e0afc848e7359c3fc9f08966de4b4fa7c4
rangapv <rangapv@yahoo.com>
2021-06-21 11:21:10 +0000

    fixed new line on echo

--- kube_adm/adm_containerd_install.sh
+++ kube_adm/adm_containerd_install.sh
@@ -14,7 +14,7 @@ kubeblock() {
 # sudo systemctl enable kubelet && sudo systemctl start kubelet
   sudo modprobe br_netfilter
   echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
-  echo "net.bridge.bridge-nf-call-ip6tables = 1 \nnet.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf
+  echo -e "net.bridge.bridge-nf-call-ip6tables = 1 \nnet.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf
   sudo sysctl --system
  sudo $cm1 install -y cowsay
 
