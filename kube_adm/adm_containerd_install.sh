b8f33e4adc13a52d7f5ea354e5a8e47df8dd1be4
rangapv <rangapv@yahoo.com>
2021-06-21 11:53:39 +0000

    Added more functions

--- kube_adm/adm_containerd_install.sh
+++ kube_adm/adm_containerd_install.sh
@@ -11,15 +11,25 @@ kubeblock() {
  sudo $cm1 update
  sudo $cm1 install -y kubectl kubeadm kubelet
  sudo apt-mark hold kubectl kubeadm kubelet
-# sudo systemctl enable kubelet && sudo systemctl start kubelet
-  sudo modprobe br_netfilter
-  echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
-  echo -e "net.bridge.bridge-nf-call-ip6tables = 1 \nnet.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf
-  echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-  sudo sysctl --system
+ sudo modprobe br_netfilter
+ echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
+ echo -e "net.bridge.bridge-nf-call-ip6tables = 1 \nnet.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf
+ echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
+ sudo sysctl --system
  sudo $cm1 install -y cowsay
 
 }
+
+kubeinit() {
+
+	echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init --config=\`wget https://gist.github.com/rangapv/3fd8a52f66bd412b1cc0663a45c74f68.git\` \n\n" | cowsay -W145 -f default
+	echo -e "Finally after the above command run this \n mkdir -p \$HOME/.kube
+\n sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
+\n sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config" | cowsay -W120 -f default
+
+}
+
+
 
 dk=$(which containerd)
 ds=$?
@@ -32,12 +42,12 @@ then
 	if [ ! -z "$d1" ]
 	then 
         kubeblock
-        echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init  --config=./kube-adm-containerd.yaml --pod-network-cidr=10.244.0.0/16 --token-ttl 0 \n\n" | cowsay -W95 -f default
+	kubeinit
         elif [ ! -z "$u1" ]
 	then
         kubeblock 
-	echo -e "ONE MORE THING ........ \n execute the below command and capture the node tokens\n sudo kubeadm init --config=\`wget https://gist.github.com/rangapv/3fd8a52f66bd412b1cc0663a45c74f68.git\` \n\n" | cowsay -W95 -f default
-	fi 
+	kubeinit
+        fi 
 else
    echo "Pls install containerd and re-run this Script"
 fi
