30a47b8bd4794e2f9a2b320f203406508926b476
rangapv <rangapv@yahoo.com>
2021-06-27 06:22:57 +0000

    Added script to install flannel and dashboard

---
+++ kube_adm/k8sfladash.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -E
+source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh)
+
+kubeflannel() {
+
+  kubectl apply -f https://raw.githubusercontent.com/rangapv/k8s/master/kube_flannel/kube-flannel.yml
+
+
+}
+
+kubedash() {
+
+  kubectl apply -f https://raw.githubusercontent.com/rangapv/k8s/master/kube_dash/recommended.yaml
+  kubectl apply -f https://raw.githubusercontent.com/rangapv/k8s/master/kube_dash/dashboard-adminuser.yaml
+
+}
+
+kc=`which kubectl`
+ks= echo "$?"
+if [[ $ks -eq 0 ]]
+then
+     kubeflannel
+     kubedash
+else
+	echo "kbectl is not installed or cofigured"
+fi
