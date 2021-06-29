af91de385546d1239018e7d42554098202170a44
rangapv <rangapv@yahoo.com>
2021-06-29 14:32:51 +0000

    Adding script to install flannel on the nodes

---
+++ kube_node/k8snodeflanel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -E
+source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh)
+
+kubeflannel() {
+
+  kubectl apply -f https://raw.githubusercontent.com/rangapv/k8s/master/kube_flannel/kube-flannel.yml
+
+
+}
+
+
+kc=`which kubectl`
+ks= echo "$?"
+if [[ $ks -eq 0 ]]
+then
+     kubeflannel
+     kubedash
+else
+        echo "kbectl is not installed or cofigured"
+fi
+
