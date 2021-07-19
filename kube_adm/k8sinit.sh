3084f432f9f0b40355847451dc3a665c992c6737
rangapv <rangapv@yahoo.com>
2021-07-19 08:29:48 +0000

    Add the kubeblock function()

--- kube_adm/k8sinit.sh
+++ kube_adm/k8sinit.sh
@@ -4,6 +4,25 @@ source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.s
 source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/checkruntime.sh)
 kubecount=0
 
+kubeblock() {
+
+ add2="$@"
+ sudo $cm1 update && sudo $cm1 install -y apt-transport-https ca-certificates
+ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+ sudo $cm1 update
+ sudo $cm1 install -y kubectl kubeadm kubelet
+ sudo apt-mark hold kubectl kubeadm kubelet
+ sudo modprobe br_netfilter
+ echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
+ echo -e "net.bridge.bridge-nf-call-ip6tables = 1 \nnet.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf
+ echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
+ sudo sysctl --system
+ sudo $cm1 install -y cowsay
+
+}
+
+
 kubegist() {
 
    if [[ -f "adm-init.yaml" ]]
@@ -63,10 +82,12 @@ then
 
 	if [ ! -z "$d1" ]
 	then
+        kubeblock
 	kubegist
 	kubeinit
         elif [ ! -z "$u1" ]
 	then
+        kubeclock
 	kubegist
 	kubeinit
         fi
