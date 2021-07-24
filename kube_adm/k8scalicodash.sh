87445a651618b58f0900dbbbd2de4c9e3082eb24
rangapv <rangapv@yahoo.com>
2021-07-24 13:22:23 +0000

    Added calicoctl install via sourcing

--- kube_adm/k8scalicodash.sh
+++ kube_adm/k8scalicodash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -E
 source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh)
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_calico/calctl.sh)
 
 kubecalico() {
 
